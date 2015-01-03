f7db7bd5a22fb102e3f24683e49736ea2444a8a9
Emanuele Raineri <emanuele.raineri@gmail.com>
2015-01-03 12:49:57 +0100

    script to check gimli's results meth_of_bed.R

--- meth_of_bed.R
+++ meth_of_bed.R
@@ -1,8 +1,23 @@
 source("libgimli.R")
 
-bed.fname <- "gencode.v19.TSS.notlow.chr1.promoters"
-meth.fname<- "~/Desktop/meth_data/C004GD51_cpg.chr1.txt.gz"
+bed.fname  <- "gencode.v19.TSS.notlow.chr1.promoters"
+meth.fname <- "~/Desktop/meth_data/C004GD51_cpg.chr1.txt.gz"
 
-meth<-libgimli$load.meth(meth.fname)
-bed<-read.table( bed.fname , stringsAsFactors=F )
+if (!exists("meth")){
+	meth<-libgimli$load.meth(meth.fname)
+	bed<-read.table( bed.fname , stringsAsFactors=F )
+}
 
+meth.rd    <- libgimli$rd.of.meth(meth,2,c(6,7))
+#lb<-bed[2,2]
+#ub<-bed[2,3]
+#print(lb)
+#print(ub)
+#lb<-lb-1000
+#ub<-ub+1000
+lb<-64431711-500
+ub<-64432082+500
+meth.slice <- libgimli$take.slice(meth.rd,lb,ub)
+plot(start(meth.slice$ranges),meth.slice$nc/(meth.slice$nc+meth.slice$c),
+	col="red",type='b',ylim=c(0,1))
+abline(v=c(64431711,64432082),col="blue")	
