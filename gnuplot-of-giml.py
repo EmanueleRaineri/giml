8d29ed225a471975be84079fbc0e68f01b761ba1
Emanuele <emanuele.raineri@gmail.com>
2017-04-06 17:55:17 +0200

    modify plotting script, so that it can plot only segments

--- gnuplot-of-giml.py
+++ gnuplot-of-giml.py
@@ -7,21 +7,37 @@ try:
 	methfn=sys.argv[1]
 	gimlfn=sys.argv[2]
 	lam=sys.argv[3]
+	mode=sys.argv[4]
 except:
-	sys.stderr.write("usage: <meth-file> <giml-file> <lambda>\n")
+	sys.stderr.write(
+		"usage: <meth-file> <giml-file> <lambda> <mode(all/segs)>\n")
 	sys.exit(1)
 
 gimlf=open(gimlfn)
 arrows=""
+xmin=float('+inf')
+xmax=float("-inf")
 for line in gimlf:
 	fields=line.split("\t")
 	if (fields[3]!=lam):continue
 	level=float(fields[6])
 	col="blue"
+	if (float(fields[1])<xmin):
+		xmin=float(fields[1])
+	if (float(fields[2])>xmax):
+		xmax=float(fields[2])
 	arrows=arrows+"set arrow from %s,%s to %s,%s nohead lw 1.5 lc rgb '%s'\n"%\
 	(fields[1],level,fields[2],level,col)
 
 sys.stdout.write(arrows)
 sys.stdout.write("set key bmargin box\nset yra[0:1]\nset grid\n")
-cmd="plot \"%s\" u 2:($3/($3+$4)) pt 7 ps 0.7  lc rgb \"%s\"\n"%(methfn,"red")
+sys.stdout.write("set xra[%f:%f]\n"%(xmin,xmax))
+if ("all"==mode):
+	cmd="plot \"%s\" u 2:($3/($3+$4)) pt 7 ps 0.7  lc rgb \"%s\" w dots\n"%\
+		(methfn,"red")
+elif ("segs"==mode):
+	cmd="plot 1/0\n"
+else:
+	sys.stderr.write("bon't know about mode %s\n"%(mode))
+	sys.exit(1)
 sys.stdout.write(cmd)
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-TESTFILE=C001UYA3bs_cpg.chr1.counts.slice.txt
-time -p ./giml ${TESTFILE} 1:10:100:1000 2> ${TESTFILE}.log > ${TESTFILE}.giml 
+TESTFILE=C001UYA3bs-cpg-chr1-counts-slice
+time -p ./giml ${TESTFILE}.txt 1:10:100:1000 2> ${TESTFILE}.log > ${TESTFILE}.giml 
